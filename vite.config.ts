2bede10b47b975011cc75c356cce9e56c3871884
olubisiayantoye <olubisiayantoye@gmail.com>
2026-03-20 17:15:07 +0100

    modified:   vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
   server: {
-    host: true,
-    allowedHosts: 'all'
+    host: true, // or '0.0.0.0'
+    port: 5173,
+    allowedHosts: ['communitypulse-byu.onrender.com']
   }
-});
+})
